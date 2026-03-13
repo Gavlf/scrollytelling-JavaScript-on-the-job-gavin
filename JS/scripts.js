75034085483b1b7061d4614612b2720e1f52e1c0
Gavin Figueroa <gfigueroa@ewu.edu>
2026-03-13 06:36:57 +0000

    Begin GSAP

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,31 @@
 // Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
+const heroHeadings = gsap.utils.toArray("#hero h1");
+const mainScene = document.querySelector("main");
+
+if (heroHeadings.length && mainScene) {
+	gsap.fromTo(
+		heroHeadings,
+		{
+			y: -220,
+			opacity: 0,
+		},
+		{
+			y: 0,
+			opacity: 1,
+			ease: "none",
+			stagger: 0.08,
+			scrollTrigger: {
+				trigger: mainScene,
+				start: "top top",
+				end: "+=500",
+				scrub: true,
+				pin: true,
+				anticipatePin: 1,
+			},
+		}
+	);
+}
+
 
